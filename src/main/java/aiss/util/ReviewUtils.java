f53db6888d16a5800c7d10ee86ec7c7e3eb2bc45
Elena Tomás Vela <eletomvel@alum.us.es>
2022-05-25 19:02:39 +0200

    Added review sorting and filtering functionality (not implemented yet)

---
+++ src/main/java/aiss/util/ReviewUtils.java
@@ -0,0 +1,17 @@
+package aiss.util;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import aiss.model.Review;
+
+public class ReviewUtils {
+	
+	public static List<Review> filterReviews(Collection<Review> reviews, String filter) {
+		return reviews.stream()
+				.filter(Filtering.parseReviewFilter(filter))
+				.collect(Collectors.toList());
+	}
+	
+}
